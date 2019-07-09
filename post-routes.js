13b9e47bd91ba34bd94e547446d26cd5b8d2600d
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-09 16:24:11 +0400

    add findById route

--- post-routes.js
+++ post-routes.js
@@ -1,14 +1,25 @@
 const express = require("express");
-const Db = require("./db");
+const Db = require("./data/db");
 const route = express.Router();
 
-route.get("api/posts", (req, res) => {
+route.get("/", (req, res) => {
   Db.find()
     .then(res => {
       res.status(200).json(res);
     })
     .catch(err => {
       res.status(500).json({ message: "Error retrieving posts" });
+    });
+});
+
+route.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Db.findById(id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error retrieving the post" });
     });
 });
 
